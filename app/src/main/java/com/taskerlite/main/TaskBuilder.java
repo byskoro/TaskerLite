a883970dd8cd3bf4e40122dbebff736b5856b772
roman <roman.bysko@emtcontrols.com>
2015-02-15 23:43:43 +0200

    add

--- app/src/main/java/com/taskerlite/main/TaskBuilder.java
+++ app/src/main/java/com/taskerlite/main/TaskBuilder.java
@@ -1,12 +1,17 @@
 package com.taskerlite.main;
 
 import com.taskerlite.R;
+import com.taskerlite.other.ScreenConverter;
 import com.taskerlite.other.Vibro;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,6 +19,11 @@ public class TaskBuilder extends Fragment {
 
 	TaskerBuilderView taskerView;
     Context context;
+
+    private Bitmap background;
+    private Bitmap picture;
+
+    private int sizeIcon;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,26 +34,48 @@ public class TaskBuilder extends Fragment {
 		taskerView = (TaskerBuilderView) view.findViewById(R.id.dot);
         taskerView.setViewCallBack(viewCallBack);
 
+        background = BitmapFactory.decodeResource(getResources(), R.drawable.button);
+
 		return view;
 	}
 
     TaskerBuilderView.ViewCallBack viewCallBack = new TaskerBuilderView.ViewCallBack(){
 
         @Override
-        public void shortPress() {
+        public void shortPress(MotionEvent event) {
+
             Vibro.playShort(context);
         }
 
         @Override
-        public void longPress() {
+        public void longPress(MotionEvent event) {
+
             Vibro.playLong(context);
         }
 
         @Override
-        public void movement() {
+        public void movement(MotionEvent event) {
+
             Vibro.playMovement(context);
             taskerView.postInvalidate();
         }
+
+        @Override
+        public void drawView(Canvas canvas, MotionEvent event) {
+
+            try{
+
+                canvas.drawColor(0xffff00ff);
+                canvas.drawBitmap(picture, event.getX(), event.getY(), null);
+
+            }catch (Exception e){ }
+        }
+
+        @Override
+        public void prepareResource(int w, int h) {
+
+            sizeIcon = w / 5;
+            picture = Bitmap.createScaledBitmap(background, sizeIcon, sizeIcon, true);
+        }
     };
-	
 }
--- app/src/main/java/com/taskerlite/main/TaskerBuilderView.java
+++ app/src/main/java/com/taskerlite/main/TaskerBuilderView.java
@@ -1,96 +1,72 @@
 package com.taskerlite.main;
 
-import com.taskerlite.other.ScreenConverter;
-import com.taskerlite.other.Vibro;
-
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class TaskerBuilderView extends View{
-	
-    Context context;
 
-    private float x = 0;
-    private float y = 0;
-    private Paint myPaint;
-    private Paint backgroundPaint;
+    private ViewCallBack viewCallBack;
+    private MotionEvent event;
 
     private float mDownX;
     private float mDownY;
-    private float SCROLL_THRESHOLD = 10;
+    private float scrollThreshold = 10;
     private boolean isOnClick;
 
     interface ViewCallBack{
-        public void shortPress();
-        public void longPress();
-        public void movement();
+        public void shortPress(MotionEvent event);
+        public void longPress(MotionEvent event);
+        public void movement(MotionEvent event);
+        public void drawView(Canvas canvas, MotionEvent event);
+        public void prepareResource(int w, int h);
     }
-
-    ViewCallBack viewCallBack;
 
     public TaskerBuilderView(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-        this.context = context;
-        
-        backgroundPaint = new Paint();
-        backgroundPaint.setColor(Color.BLUE);
-
-        myPaint = new Paint();
-        myPaint.setColor(Color.WHITE);
-        myPaint.setAntiAlias(true);
     }
 
     public void setViewCallBack(ViewCallBack viewCallBack){
         this.viewCallBack = viewCallBack;
     }
-    
-    final Handler handler = new Handler(); 
-    Runnable mLongPressed = new Runnable() {
-        public void run() {
-            isOnClick = false;
-            viewCallBack.longPress();
-        }   
-    };
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
+        this.event = event;
+
     	switch (event.getAction() & MotionEvent.ACTION_MASK) {
     		case MotionEvent.ACTION_DOWN:
 
-    			mDownX = event.getX(); 
-    			mDownY = event.getY(); 
+    			mDownX = event.getX();
+    			mDownY = event.getY();
+
     			isOnClick = true;
-                handler.postDelayed(mLongPressed, 1000);
+
+                longPressDetect.sendEmptyMessageDelayed(0, 1000);
 
                 break;
     		case MotionEvent.ACTION_CANCEL:
     		case MotionEvent.ACTION_MOVE:
 
-    			if ((Math.abs(mDownX - event.getX()) > SCROLL_THRESHOLD ||Math.abs(mDownY - event.getY()) > SCROLL_THRESHOLD)) {
+    			if ((Math.abs(mDownX - event.getX()) > scrollThreshold ||Math.abs(mDownY - event.getY()) > scrollThreshold)) {
 
     				isOnClick = false;
-                    handler.removeCallbacks(mLongPressed);
-                    viewCallBack.movement();
-
-                    x = event.getX();
-                    y = event.getY();
+                    longPressDetect.removeMessages(0);
+                    viewCallBack.movement(event);
     			}
 
     			break; 
     		case MotionEvent.ACTION_UP:
 
-                handler.removeCallbacks(mLongPressed);
+                longPressDetect.removeMessages(0);
 
     			if (isOnClick)
-                    viewCallBack.shortPress();
+                    viewCallBack.shortPress(event);
 
     			break; 
     		}  
@@ -98,11 +74,21 @@ public class TaskerBuilderView extends View{
     	return true;
     }
 
-	public void draw(Canvas canvas) {
-		
-		int width = canvas.getWidth();
-		int height = canvas.getHeight();
-		canvas.drawRect(0, 0, width, height, backgroundPaint);
-        canvas.drawRect(x, y, x + ScreenConverter.dp2px(context, 56), y + ScreenConverter.dp2px(context, 56), myPaint);
+    Handler longPressDetect = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            isOnClick = false;
+            viewCallBack.longPress(event);
+        };
+    };
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        viewCallBack.prepareResource(w, h);
+    }
+
+    public void draw(Canvas canvas) {
+        viewCallBack.drawView(canvas, event);
 	}
 }
