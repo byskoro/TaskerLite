f437d2c6acb8157aa0fc52c18669237c1937b6ec
roman <roman.bysko@emtcontrols.com>
2015-02-17 01:26:10 +0200

    add

--- app/src/main/java/com/taskerlite/main/TaskerService.java
+++ app/src/main/java/com/taskerlite/main/TaskerService.java
@@ -101,6 +101,7 @@ public class TaskerService extends Service {
     		
     		if(hour==24)
         		hour = 0;
+            // need add ++day
     	}
     	cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), hour, minutes, 0);
     	
