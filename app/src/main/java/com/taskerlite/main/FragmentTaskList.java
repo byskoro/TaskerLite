f3ad3a31b89af6a625b7d15fcb6115b2a5772d6c
roman <roman.bysko@emtcontrols.com>
2015-02-21 00:45:20 +0200

    add

--- app/src/main/java/com/taskerlite/main/FragmentTaskList.java
+++ app/src/main/java/com/taskerlite/main/FragmentTaskList.java
@@ -22,7 +22,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
